7c1468fb0509311d67f875a4685eb4519df32334
cheese <413899914@qq.com>
2024-06-04 17:03:36 +0800

    tag plugins finally (with GD's multi sheets)

--- tools/sidekick/plugins/tags/tags.js
+++ tools/sidekick/plugins/tags/tags.js
@@ -11,7 +11,7 @@
  */
 
 // eslint-disable-next-line import/no-unresolved
-import { PLUGIN_EVENTS } from 'https://main--edge-delivery-demo--zhangqx1994.hlx.page/tools/sidekick/library/events/events.js';
+import { PLUGIN_EVENTS } from '/tools/sidekick/library/events/events.js';
 
 const selectedTags = [];
 
